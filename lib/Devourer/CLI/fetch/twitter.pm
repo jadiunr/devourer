7bc7916825ffc56a9ab60cc5309a41f28a88c816
jadiunr <jadiunr@gmail.com>
2021-06-25 07:43:52 +0900

    たぶんうごく

--- lib/Devourer/CLI/fetch/twitter.pm
+++ lib/Devourer/CLI/fetch/twitter.pm
@@ -27,9 +27,9 @@ has saved_files => (is => 'ro', lazy => 1, default => sub {
     my $self = shift;
     [map {basename($_)} (split /\n/, `find @{[$self->settings->{outdir}]} -type f`)];
 });
-has saved_statuses => (is => 'ro', lazy => 1, default => sub {
+has favorited_statuses => (is => 'ro', lazy => 1, default => sub {
     my $self = shift;
-    my $statuses = [map {(split '-', basename($_))[0]} (split /\n/, `find @{[$self->settings->{outdir}]} -type d -name all -prune -o -type f -a -mtime -30 -print`)];
+    my $statuses = [map {(split '-', basename($_))[0]} (split /\n/, `find @{[$self->settings->{outdir}]}/favorited -type f -a -mtime -30 -print`)];
     $statuses = [grep {$_ =~ /^\d+$/} @$statuses];
     return [sort {$b <=> $a} @$statuses];
 });
@@ -68,41 +68,21 @@ sub _standard_fetch {
     my $self = shift;
 
     my $mediators = $self->settings->{mediators};
-    my $all_statuses = [];
+    my $all_statuses;
 
-    {
-        my $max_id;
-        for my $iter (1..4) {
-            my $timeline;
-            $timeline = $self->twitter->home_timeline({count => 200}) if !defined($max_id);
-            $timeline = $self->twitter->home_timeline({count => 200, max_id => $max_id}) if defined($max_id);
-            push(@$all_statuses, @$timeline);
-            $max_id = $timeline->[-1]{id};
-        }
-    }
+    my $timeline = $self->_get_home_timeline();
+    push(@$all_statuses, @$timeline);
 
     for my $mediator (@$mediators) {
-        my $max_id;
-        for my $iter (1..5) {
-            my $favorites;
-            $favorites = $self->twitter->favorites({screen_name => $mediator, count => 200}) if !defined($max_id);
-            $favorites = $self->twitter->favorites({screen_name => $mediator, count => 200, max_id => $max_id}) if defined($max_id);
-            push(@$all_statuses, @$favorites);
-            $max_id = $favorites->[-1]{id};
-        }
+        my $favorites = $self->_get_favorites();
+        push(@$all_statuses, @$favorites);
     }
 
-    my %tmp;
-    my $unique_statuses = [grep {!$tmp{$_->{id}}++} @$all_statuses];
-    my $sorted_statuses = [sort {
-        Time::Piece->strptime($a->{created_at}, '%a %b %d %T %z %Y')
-        <=>
-        Time::Piece->strptime($b->{created_at}, '%a %b %d %T %z %Y')
-    } @$unique_statuses];
+    my $sorted_statuses = $self->_sort_and_uniq_statuses($all_statuses);
 
     for my $status (@$sorted_statuses) {
         my $media_array = $status->{extended_entities}{media};
-        $self->download($media_array, $status->{id}) if $media_array;
+        $self->_download($media_array, $status->{id}) if $media_array;
     }
 
     my $rate_limit_status = $self->twitter->rate_limit_status()->{resources};
@@ -114,30 +94,13 @@ sub _standard_fetch {
 sub _fetch_from_users {
     my ($self, $users) = @_;
 
-    my $all_statuses = [];
-
     for my $user (@$users) {
-        my $max_id;
-        for my $iter (1..16) {
-            my $statuses;
-            $statuses = $self->twitter->user_timeline({screen_name => $user, count => 200}) if !defined($max_id);
-            $statuses = $self->twitter->user_timeline({screen_name => $user, count => 200, max_id => $max_id}) if defined($max_id);
-            push(@$all_statuses, @$statuses);
-            $max_id = $statuses->[-1]{id};
-            last if scalar(@$statuses) < 200;
-        }
-
-        my %tmp;
-        my $unique_statuses = [grep {!$tmp{$_->{id}}++} @$all_statuses];
-        my $sorted_statuses = [sort {
-            Time::Piece->strptime($a->{created_at}, '%a %b %d %T %z %Y')
-            <=>
-            Time::Piece->strptime($b->{created_at}, '%a %b %d %T %z %Y')
-        } @$unique_statuses];
+        my $statuses = $self->opts->{fav} ? $self->_get_favorites($user) : $self->_get_user_timeline($user);
+        my $sorted_statuses = $self->_sort_and_uniq_statuses($statuses);
 
         for my $status (@$sorted_statuses) {
             my $media_array = $status->{extended_entities}{media};
-            $self->download($media_array, $status->{id}) if $media_array;
+            $self->_download($media_array, $status->{id}) if $media_array;
         }
 
         my $rate_limit_status = $self->twitter->rate_limit_status()->{resources};
@@ -162,7 +125,7 @@ sub _devour {
             
             for my $user_status (@$user_statuses) {
                 my $media_array = $user_status->{extended_entities}{media};
-                $self->download($media_array, $user_status->{id}) if $media_array;
+                $self->_download($media_array, $user_status->{id}) if $media_array;
             }
 
             my $rate_limit_status = $self->twitter->rate_limit_status()->{resources};
@@ -177,7 +140,70 @@ sub _devour {
     }
 }
 
-sub download {
+sub _get_home_timeline {
+    my $self = shift;
+
+    my $all_statuses;
+    my $max_id;
+    for my $iter (1..4) {
+        my $timeline;
+        $timeline = $self->twitter->home_timeline({count => 200}) if !defined($max_id);
+        $timeline = $self->twitter->home_timeline({count => 200, max_id => $max_id}) if defined($max_id);
+        push(@$all_statuses, @$timeline);
+        $max_id = $timeline->[-1]{id};
+    }
+
+    return $all_statuses;
+}
+
+sub _get_favorites {
+    my ($self, $user) = @_;
+
+    my $all_statuses;
+    my $max_id;
+    for my $iter (1..5) {
+        my $favorites;
+        $favorites = $self->twitter->favorites({screen_name => $user, count => 200}) if !defined($max_id);
+        $favorites = $self->twitter->favorites({screen_name => $user, count => 200, max_id => $max_id}) if defined($max_id);
+        push(@$all_statuses, @$favorites);
+        $max_id = $favorites->[-1]{id};
+    }
+
+    return $all_statuses;
+}
+
+sub _get_user_timeline {
+    my ($self, $user) = @_;
+
+    my $all_statuses;
+    my $max_id;
+    for my $iter (1..16) {
+        my $statuses;
+        $statuses = $self->twitter->user_timeline({screen_name => $user, count => 200}) if !defined($max_id);
+        $statuses = $self->twitter->user_timeline({screen_name => $user, count => 200, max_id => $max_id}) if defined($max_id);
+        push(@$all_statuses, @$statuses);
+        $max_id = $statuses->[-1]{id};
+        last if scalar(@$statuses) < 200;
+    }
+
+    return $all_statuses;
+}
+
+sub _sort_and_uniq_statuses {
+    my ($self, $statuses) = @_;
+
+    my %tmp;
+    my $unique_statuses = [grep {!$tmp{$_->{id}}++} @$statuses];
+    my $sorted_statuses = [sort {
+        Time::Piece->strptime($a->{created_at}, '%a %b %d %T %z %Y')
+        <=>
+        Time::Piece->strptime($b->{created_at}, '%a %b %d %T %z %Y')
+    } @$unique_statuses];
+
+    return $sorted_statuses;
+}
+
+sub _download {
     my $self = shift;
     my $media_array = shift;
     my $status_id = $media_array->[0]{source_status_id} ? $media_array->[0]{source_status_id} : shift;
@@ -191,7 +217,7 @@ sub download {
 
         my $filename = $status_id."-".basename($url);
 
-        if (grep {$filename eq $_} @{$self->saved_files} or -f "@{[$self->settings->{outdir}]}/$filename") {
+        if (grep {$filename eq $_} @{$self->saved_files} or -f "@{[$self->settings->{outdir}]}/searching/$filename") {
             say "[@{[ localtime->datetime ]}]Already saved     : $filename";
             return;
         }
@@ -199,13 +225,13 @@ sub download {
         $binary = $self->http->get($url);
         warn "[@{[ localtime->datetime ]}]Cannot fetch video: returned " . $binary->code . ", url: $url" and return
             if grep {$_ eq $binary->code} (404, 500);
-        $self->save($filename, $binary);
+        $self->_save($filename, $binary);
     } else {
         for my $image (@$media_array) {
             my $url = $image->{media_url};
 
             my $filename = $status_id."-".basename($url);
-            if (grep {$filename eq $_} @{$self->saved_files} or -f "@{[$self->settings->{outdir}]}/$filename") {
+            if (grep {$filename eq $_} @{$self->saved_files} or -f "@{[$self->settings->{outdir}]}/searching/$filename") {
                 say "[@{[ localtime->datetime ]}]Already saved     : $filename";
                 return;
             }
@@ -213,15 +239,15 @@ sub download {
             $binary = $self->http->get($url.'?name=orig');
             warn "[@{[ localtime->datetime ]}]Cannot fetch image: returned " . $binary->code . ", url: $url" and next
                 if grep {$_ eq $binary->code} (404, 500);
-            $self->save($filename, $binary);
+            $self->_save($filename, $binary);
         }
     }
 }
 
-sub save {
+sub _save {
   my ($self, $filename, $binary) = @_;
 
-  open my $fh, ">", "./@{[$self->settings->{outdir}]}/$filename"
+  open my $fh, ">", "./@{[$self->settings->{outdir}]}/searching/$filename"
     or die "[@{[ localtime->datetime ]}]Cannot create file: $!, filename: ".$filename;
   say $fh $binary->content;
   close $fh;
